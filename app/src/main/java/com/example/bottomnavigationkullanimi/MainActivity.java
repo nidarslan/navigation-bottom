ce9a7bb6b51d04ffb323648c66d7452fc4079d05
Nida Arslan <miinidars@gmail.com>
2022-10-25 21:47:39 +0300

    bottom

---
+++ app/src/main/java/com/example/bottomnavigationkullanimi/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.bottomnavigationkullanimi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNav;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNav = findViewById(R.id.bottomNav);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+
+        //baglanti
+        NavigationUI.setupWithNavController(bottomNav,navHostFragment.getNavController());
+    }
+}
